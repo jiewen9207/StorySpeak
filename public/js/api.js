2b53a1d6c5bb5f7ec2d52c371058a3de25e2f8d0
Daniel <ddeng5889@gmail.com>
2026-07-30 11:26:09 +0800

    fix: update API URL to use workers.dev

--- public/js/api.js
+++ public/js/api.js
@@ -1,6 +1,4 @@
-const API_BASE = window.location.hostname.includes('pages.dev') 
-  ? 'https://storyspeak-worker.workers.dev' 
-  : '';
+const API_BASE = 'https://storyspeak-worker.ddeng5889.workers.dev';
 
 function getToken() {
   return localStorage.getItem('storyspeak_token');
